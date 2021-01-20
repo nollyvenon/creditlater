b40688044f61b591ff679187685107e76d8dd61a
John Azuka <gooption@yahoo.com>
2021-01-21 01:20:49 +0200

    Delete resources/views/web directory

--- resources/views/web/alerts/alert-success.blade.php
+++
@@ -1,7 +0,0 @@
-
-
-<!-- cart alert  error -->
-<div class="alert-content-bar">{{ Session::get('alert') }}</div>
-
-<!-- alert content success -->
-<div class="alert-success-content">{{ Session::get('alert-success') }}</div>
